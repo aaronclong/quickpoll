a53d9f8357bf6e411bdfbb1df7e379d15f54f955
Aaron Long <aaron.long.c@gmail.com>
2017-06-19 22:48:53 -0400

    Added repositories

---
+++ src/main/java/io/zipcoder/aaronclong/quickpoll/modal/OptionRepository.java
@@ -0,0 +1,10 @@
+package io.zipcoder.aaronclong.quickpoll.modal;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+/**
+ * Created by aaronlong on 6/19/17.
+ */
+public interface OptionRepository extends CrudRepository<Poll, Long> {
+}
---
+++ src/main/java/io/zipcoder/aaronclong/quickpoll/modal/PollRepository.java
@@ -0,0 +1,9 @@
+package io.zipcoder.aaronclong.quickpoll.modal;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by aaronlong on 6/19/17.
+ */
+public interface PollRepository extends CrudRepository<Poll, Long> {
+}
